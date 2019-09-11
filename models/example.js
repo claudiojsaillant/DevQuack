e4dfccfe99691f29d7ce1aaaff8e71fd2617398f
Cloud <claudiojsaillant@gmail.com>
2019-09-10 20:13:38 -0400

    exampl

--- models/example.js
+++ models/example.js
@@ -1,5 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
+  var Example = sequelize.define("Exampl", {
     text: DataTypes.STRING,
     description: DataTypes.TEXT
   });

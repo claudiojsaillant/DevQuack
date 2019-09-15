ce66a8b53fcc8c17b0c3c19e636608154650eabd
Cloud <claudiojsaillant@gmail.com>
2019-09-15 09:35:09 -0400

    Html / Api routes working perfectly

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,19 +11,58 @@ module.exports = function(app) {
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
+  app.get("/welcome/:userid", function(req, res) {
+    var userId = req.params.userid;
+    db.Users.findAll({
+      where: {
+        id: userId
+      }
+    }).then(function(userData) {
+      res.render("logged", {
+        user: userData
       });
     });
   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+  app.get("/user/:username", function(req, res) {
+    var username = req.params.username;
+    db.Users.findAll({
+      where: {
+        username: username
+      }
+    }).then(function(userData) {
+      if (userData.length != 0) {
+        res.render("profile", {
+          msg: "Welcome " + userData[0].firstName + " " + userData[0].lastName,
+          user: userData
+        });
+      }
+    });
   });
-};
+
+  app.get("/newpost/:userId", function(req, res) {
+    var userId = req.params.userId;
+    db.Users.findAll({
+      where: {
+        id: userId
+      }
+    })
+    
+  })
+
+  // Load example page and pass in an example by id
+  // app.get("/example/:id", function(req, res) {
+  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(
+  //     dbExample
+  //   ) {
+  //     res.render("example", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
+
+  // Render 404 page for any unmatched routes
+//   app.get("*", function(req, res) {
+//     res.render("404");
+//   });
+// };
---
+++ routes/postRoutes.js
@@ -0,0 +1,93 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  // All posts with comments, users and category
+  app.get("/api/posts", function(req, res) {
+    db.Posts.findAll({
+      include: [db.Categories, db.Comments, db.Users]
+    }).then(function(data) {
+      res.json(data);
+    });
+  });
+
+  // Specific post with comments and users and category
+  app.get("/api/posts/:id", function(req, res) {
+    var postId = req.params.id;
+    db.Posts.findAll({
+      where: {
+        id: postId
+      },
+      include: [db.Categories, db.Comments, db.Users]
+    }).then(function(data) {
+      res.json(data);
+    });
+  });
+
+  // Posts by category with comments and users
+  app.get("/api/posts/category/:categoryid/", function(req, res) {
+    var categoryId = req.params.categoryid;
+    db.Posts.findAll({
+      where: {
+        CategoryId: categoryId
+      },
+      include: [db.Categories, db.Comments, db.Users]
+    }).then(function(data) {
+      res.json(data);
+    });
+  });
+
+  // Posts by users in category
+  app.get("/api/posts/category/:categoryid/user/:userid", function(req, res) {
+    var categoryId = req.params.categoryid;
+    var userId = req.params.userid;
+    // var category = req.params.category;
+    db.Posts.findAll({
+      where: {
+        UserId: userId,
+        CategoryId: categoryId
+      },
+      include: [db.Categories, db.Comments, db.Users]
+    }).then(function(data) {
+      res.json(data);
+    });
+  });
+
+  // Create new post
+  app.post("/newpost/:userId", function(req, res) {
+    db.Posts.create({
+      content: req.body.content,
+      title: req.body.title,
+      profileUrl: req.body.profileUrl,
+      CategoryId: req.body.CategoryId,
+      UserId: req.body.UserId,
+      stars: 0
+    }).then(function(data) {
+      res.json(data);
+    });
+  });
+
+  // Update post
+  app.put("/api/posts", function(req, res) {
+    db.Posts.update(
+      {
+        title: req.body.title,
+        content: req.body.content,
+        CategoryId: req.body.categoryId
+      },
+      {
+        where: {
+          id: req.body.id
+        }
+      }
+    ).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+
+  // Delete Post
+  app.delete("/api/post/:id", function(req, res) {
+    db.Posts.destroy({ where: { id: req.params.id } }).then(function(data) {
+      res.json(data);
+    });
+  });
+};
